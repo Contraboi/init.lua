71f6a3b0c83f8dc68a18c7783d03c6b74bf690ec
Selmir Nedžibi <69647487+Contraboi@users.noreply.github.com>
2024-01-17 12:05:59 +0100

    rename

--- lua/contraboi/init.lua
+++ lua/contraboi/init.lua
@@ -1,5 +1,5 @@
-require("theprimeagen.set")
-require("theprimeagen.remap")
+require("contraboi.set")
+require("contraboi.remap")
 
 -- DO NOT INCLUDE THIS
 vim.opt.rtp:append("~/personal/streamer-tools")
